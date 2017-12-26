fe8d4e8ee7a52839cbaac55abff59b4a76200b65
Edil D'Aguila <edildaguila@outlook.com>
2017-12-26 16:26:37 -0200

    REFS #005 por Edil: Está fazendo o caminho principal do Desafio

--- desafio/src/main/java/br/uff/sti/email/Main.java
+++ desafio/src/main/java/br/uff/sti/email/Main.java
@@ -16,15 +16,16 @@ import org.apache.commons.csv.CSVRecord;
  */
 public class Main {
 
-    private static final String[] HEADERS = {"nome", "matricula", "telefone", "email", "uffmail", "status"};
+    private static final String[] HEADER = {"nome", "matricula", "telefone", "email", "uffmail", "status"};
+    private static final String DOMAIN_EMAIL = "@id.uff.br";
 
     public static void main(String[] args) throws Exception {
 
-        Reader fileReader = new FileReader("C:\\develop\\GitHub\\desafio\\desafio\\Arquivo.csv");
+        Reader fileReader = new FileReader("./Arquivo.csv");
 
         CSVParser csvParser = CSVFormat.EXCEL
                 .withFirstRecordAsHeader()
-                .withHeader(HEADERS)
+                .withHeader(HEADER)
                 .parse(fileReader);
 
         List<CSVRecord> records = csvParser.getRecords();
@@ -53,20 +54,64 @@ public class Main {
             if ("Ativo".equalsIgnoreCase(aluno.getStatus()) && aluno.getUffMail().isEmpty()) {
                 System.out.println(aluno.getNome().split(" ")[0] + ", por favor escolha uma das opções abaixo para o seu UFFMail.");
 
-                for (int i = 1; i < 6; i++) {
+                Map<Integer, String> mapa = criarMapaDeEmail(aluno.getNome());
 
-                    String texto;
-                    texto = aluno.getNome().toLowerCase();
-                    System.out.println(i + " " + "-" + " " + texto + "@id.uff.br");
-//                    for (i = 0; i < texto.length(); i++) {
-//                        System.out.println(texto.charAt(i));
-//                    }
-
+                for (Map.Entry entry : mapa.entrySet()) {
+                    System.out.println(entry.getKey() + " - " + entry.getValue());
                 }
+
+                String emailEscolhido = mapa.get(sc.nextInt());
+                if (emailEscolhido != null) {
+                    System.out.println("A criação de seu e-mail (" + emailEscolhido + ") será feita nos próximos minutos.\n"
+                            + "Um SMS foi enviado para " + aluno.getTelefone() + " com a sua senha de acesso.");
+                } else {
+                    System.out.println("Digite uma opção válida.");
+                }
+
             }
 
         }
 
+    }
+
+    private static Map<Integer, String> criarMapaDeEmail(String nome) {
+        Map<Integer, String> mapa = new HashMap<Integer, String>();
+        mapa.put(1, criarEmailSeparadoPorUnderscore(nome));
+        mapa.put(2, criarEmailComPrimeiraLetraSobrenome(nome));
+        mapa.put(3, primeiroNomeMaisSegundoNome(nome));
+        mapa.put(4, primeiraLetraMaisSobrenome(nome));
+        mapa.put(5, primeiraLetraMaisSobrenomes(nome));
+        return mapa;
+    }
+
+    private static String criarEmailSeparadoPorUnderscore(String nome) {
+        String arrayNome[] = nome.split(" ");
+        String email = arrayNome[0] + "_" + arrayNome[1] + DOMAIN_EMAIL;
+        return email.toLowerCase();
+    }
+
+    private static String criarEmailComPrimeiraLetraSobrenome(String nome) {
+        String arrayNome[] = nome.split(" ");
+        String email = arrayNome[0] + arrayNome[1].charAt(0) + arrayNome[2].charAt(0) + DOMAIN_EMAIL;
+        return email.toLowerCase();
+    }
+    
+    private static String primeiroNomeMaisSegundoNome(String nome){
+        String arrayNome[] = nome.split(" ");
+        String email = arrayNome[0] + arrayNome[1]+ DOMAIN_EMAIL;
+        return email.toLowerCase();
+    }
+    
+    private static String primeiraLetraMaisSobrenome(String nome){
+        String arrayNome[] = nome.split(" ");
+        String email = arrayNome[0].charAt(0)+ arrayNome[1] + DOMAIN_EMAIL;
+        return email.toLowerCase();
+    }
+    
+    private static String primeiraLetraMaisSobrenomes(String nome){
+        String arrayNome[] = nome.split(" ");
+        String email = arrayNome[0].charAt(0) + arrayNome[1] + arrayNome[2] + DOMAIN_EMAIL;
+        return email.toLowerCase();
     }
 
 }
