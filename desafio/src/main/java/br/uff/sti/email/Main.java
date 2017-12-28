de8278ecb0dcfd7b8bbefdd73a1a8385de884ffb
Edil D'Aguila <edildaguila@outlook.com>
2017-12-28 00:01:54 -0200

    REFS #006 por Edil: Fazendo mais algumas validações, mas ainda sem bloquear erro de tipo.

--- desafio/src/main/java/br/uff/sti/email/Main.java
+++ desafio/src/main/java/br/uff/sti/email/Main.java
@@ -1,5 +1,6 @@
 package br.uff.sti.email;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.Reader;
 import java.util.HashMap;
@@ -15,21 +16,26 @@ import org.apache.commons.csv.CSVRecord;
  * @author edil
  */
 public class Main {
+    
+//Constantes
 
     private static final String[] HEADER = {"nome", "matricula", "telefone", "email", "uffmail", "status"};
     private static final String DOMAIN_EMAIL = "@id.uff.br";
 
     public static void main(String[] args) throws Exception {
-
+//Lendo arquivo .CSV
         Reader fileReader = new FileReader("./Arquivo.csv");
 
+//Fazendo Parse do arquivo .CSV para EXCEL
         CSVParser csvParser = CSVFormat.EXCEL
                 .withFirstRecordAsHeader()
                 .withHeader(HEADER)
                 .parse(fileReader);
 
+//Criando uma lista com as linhas do .CSV
         List<CSVRecord> records = csvParser.getRecords();
 
+//Mapeando os objetos (ALUNO) com o HASHMAP
         Map<Long, Aluno> map = new HashMap<Long, Aluno>();
 
         for (CSVRecord record : records) {
@@ -43,37 +49,47 @@ public class Main {
             map.put(aluno.getMatricula(), aluno);
         }
 
+//Pedindo a matrícula do Aluno
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite sua matrícula: ");
         long matricula = sc.nextLong();
         Aluno aluno = map.get(matricula);
 
+//Testes Condicionais da Matrícula para a criação de UFFmail se necessário.
         if (aluno == null) {
             System.out.println("Digite uma matrícula válida!");
         } else {
             if ("Ativo".equalsIgnoreCase(aluno.getStatus()) && aluno.getUffMail().isEmpty()) {
-                System.out.println(aluno.getNome().split(" ")[0] + ", por favor escolha uma das opções abaixo para o seu UFFMail.");
+                System.out.println(aluno.getNome().split(" ")[0] + ", por favor escolha uma das opções abaixo para o seu UFFMail. ");
 
                 Map<Integer, String> mapa = criarMapaDeEmail(aluno.getNome());
 
                 for (Map.Entry entry : mapa.entrySet()) {
                     System.out.println(entry.getKey() + " - " + entry.getValue());
                 }
-
                 String emailEscolhido = mapa.get(sc.nextInt());
                 if (emailEscolhido != null) {
                     System.out.println("A criação de seu e-mail (" + emailEscolhido + ") será feita nos próximos minutos.\n"
-                            + "Um SMS foi enviado para " + aluno.getTelefone() + " com a sua senha de acesso.");
+                            + "Um SMS foi enviado para " + aluno.getTelefone() + " com a sua senha de acesso. ");
                 } else {
                     System.out.println("Digite uma opção válida.");
                 }
 
+            } else {
+                if ("Ativo".equalsIgnoreCase(aluno.getStatus()) && !aluno.getUffMail().equalsIgnoreCase(null)) {
+                    System.out.println("UFFMAIL já cadastrado: " + aluno.getUffMail());
+
+                }
+            }
+            if ("Inativo".equalsIgnoreCase(aluno.getStatus())) {
+                System.out.println("E-mail inativo, entre em contato com o STI");
             }
 
         }
 
     }
 
+//Mapeando a criação de E-mails e métodos de cada um dos possíveis e-mails.
     private static Map<Integer, String> criarMapaDeEmail(String nome) {
         Map<Integer, String> mapa = new HashMap<Integer, String>();
         mapa.put(1, criarEmailSeparadoPorUnderscore(nome));
@@ -95,20 +111,20 @@ public class Main {
         String email = arrayNome[0] + arrayNome[1].charAt(0) + arrayNome[2].charAt(0) + DOMAIN_EMAIL;
         return email.toLowerCase();
     }
-    
-    private static String primeiroNomeMaisSegundoNome(String nome){
+
+    private static String primeiroNomeMaisSegundoNome(String nome) {
         String arrayNome[] = nome.split(" ");
-        String email = arrayNome[0] + arrayNome[1]+ DOMAIN_EMAIL;
+        String email = arrayNome[0] + arrayNome[1] + DOMAIN_EMAIL;
         return email.toLowerCase();
     }
-    
-    private static String primeiraLetraMaisSobrenome(String nome){
+
+    private static String primeiraLetraMaisSobrenome(String nome) {
         String arrayNome[] = nome.split(" ");
-        String email = arrayNome[0].charAt(0)+ arrayNome[1] + DOMAIN_EMAIL;
+        String email = arrayNome[0].charAt(0) + arrayNome[1] + DOMAIN_EMAIL;
         return email.toLowerCase();
     }
-    
-    private static String primeiraLetraMaisSobrenomes(String nome){
+
+    private static String primeiraLetraMaisSobrenomes(String nome) {
         String arrayNome[] = nome.split(" ");
         String email = arrayNome[0].charAt(0) + arrayNome[1] + arrayNome[2] + DOMAIN_EMAIL;
         return email.toLowerCase();
